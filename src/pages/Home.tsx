f66aa856b9deacb3712e07a1b8e5bb9d48b245ce
KOTOEWai <149601802+KOTOEWai@users.noreply.github.com>
2026-03-17 09:57:46 +0630

    Add React lessons everyday i studied

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+
 const Home: React.FC = () => {
     return (
         <section style={{ padding: 'var(--spacing-xl) 0', textAlign: 'center' }}>
@@ -23,53 +24,7 @@ const Home: React.FC = () => {
                     </div>
                 ))}
             </div>
-            <div className="hover-3d">
-  {/* content */}
-  <figure className="w-60 rounded-2xl">
-    <img src="https://img.daisyui.com/images/stock/card-1.webp?x" alt="Tailwind CSS 3D card" />
-  </figure>
-  {/* 8 empty divs needed for the 3D effect */}
-  <div></div>
-  <div></div>
-  <div></div>
-  <div></div>
-  <div></div>
-  <div></div>
-  <div></div>
-  <div></div>
-</div>
-
-<div className="hover-3d">
-  {/* content */}
-  <figure className="w-60 rounded-2xl">
-    <img src="https://img.daisyui.com/images/stock/card-2.webp?x" alt="Tailwind CSS 3D hover" />
-  </figure>
-  {/* 8 empty divs needed for the 3D effect */}
-  <div></div>
-  <div></div>
-  <div></div>
-  <div></div>
-  <div></div>
-  <div></div>
-  <div></div>
-  <div></div>
-</div>
-
-<div className="hover-3d">
-  {/* content */}
-  <figure className="w-60 rounded-2xl">
-    <img src="https://img.daisyui.com/images/stock/card-3.webp?x" alt="Tailwind CSS 3D hover" />
-  </figure>
-  {/* 8 empty divs needed for the 3D effect */}
-  <div></div>
-  <div></div>
-  <div></div>
-  <div></div>
-  <div></div>
-  <div></div>
-  <div></div>
-  <div></div>
-</div>
+        
         </section>
     );
 };
